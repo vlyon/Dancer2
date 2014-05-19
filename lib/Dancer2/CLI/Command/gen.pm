142faecdd93cb4060bb5335daccc3860c56ced8c
Sawyer X <xsawyerx@cpan.org>
2014-05-19 21:42:22 +0200

    Use HTTP::Tiny instead of LWP::UserAgent in Dancer2::CLI::Command::gen:
    This will help remove LWP::UserAgent entirely, which is an added
    dependency. HTTP::Tiny is available in core since perl 5.13.9.
    
    GH #588 mentions this.
    
    Now only the tests remain.

--- lib/Dancer2/CLI/Command/gen.pm
+++ lib/Dancer2/CLI/Command/gen.pm
@@ -5,7 +5,7 @@ use strict;
 use warnings;
 
 use File::Find;
-use LWP::UserAgent;
+use HTTP::Tiny;
 use File::Path 'mkpath';
 use File::Spec::Functions;
 use File::ShareDir 'dist_dir';
@@ -257,12 +257,12 @@ Please check http://search.cpan.org/dist/Dancer2/ for updates.
 sub _send_http_request {
     my $url = shift;
 
-    my $ua = LWP::UserAgent->new;
-    $ua->timeout(5);
-    $ua->env_proxy();
+    my $ua = HTTP::Tiny->new(
+        timeout => 5,
+    );
 
     my $response = $ua->get($url);
-    return $response->is_success ? $response->content : undef;
+    return $response->{'success'} ? $response->{'content'} : undef;
 }
 
 1;
